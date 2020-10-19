fa25c1a98c732493f0a9de2d432a079678a1ac9e
Naoya Hatayama <applepedlar@gmail.com>
2020-10-19 18:58:15 +0900

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+curl -o entry_list.tsv "https://docs.google.com/spreadsheets/d/e/2PACX-1vQcih5HM_pBtmkE8je2N4ulhim9UvAWMG2xb5g2Okz5M2BWw6mUEljaaSm3sj3yLqG79QtL9vx4uoll/pubhtml?gid=0&single=true"
+git commit -a -m "update"
+git push
